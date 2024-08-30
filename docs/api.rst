d3da6326ad75e2aef7f9a4c39acbb864166062a6
Greg T. Chism <gchism@email.arizona.edu>
2024-08-30 23:26:44 +0000

    Add documentation

---
+++ docs/api.rst
@@ -0,0 +1,10 @@
+Welcome to gh_manager's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   introduction  
+   usage         
+   api           
---
+++ docs/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../gh_manager'))  # Adjust if necessary
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'gh_manager'
+copyright = '2024, Greg Chism'
+author = 'Greg Chism'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For Google and NumPy style docstrings
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_theme_options = {
+    'show_prev_next': False,
+    'show_toc_level': 2,
+}
+
+html_static_path = ['_static']
